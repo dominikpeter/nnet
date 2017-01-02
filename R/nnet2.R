85f4e25839db163186c0a7fab47e04ecbb033bbf
dominikpeter <dominik.peter87@gmail.com>
2017-01-02 15:11:58 +0100

    minor

--- R/nnet2.R
+++ R/nnet2.R
@@ -1,4 +1,7 @@
 
+set.seed(2323)
+
+
 train <- function(model, ...) UseMethod("train")
 query <- function(model, ...) UseMethod("query")
 
@@ -84,110 +87,4 @@ train.nnet <- function(model, inputs_list, target_list, learningrate) {
 predict.nnet <- function(model, newdata) {
   query(model, newdata)$final_outputs
 }
-
-
-
-# Testing
-# --------------------------------------------------------------------------------------------------
-
-
-model <- nnet(4,3,2, "sigmoid")
-model$weights
-
-
-query(model, c(1,2,3,4))
-
-
-
-train(model, 1:4, 1:2, 0.1)
-model$weights
-
-# Test Model
-# --------------------------------------------------------------------------------------------------
-
-library(magrittr)
-library(scales)
-
-input <- mtcars[, 1:7] %>% t %>% unname %>% rescale
-output <- mtcars[, "am"]
-output <- rbind(t(output), -1 %*% output + 1)
-row.names(output) <- c(0,1)
-
-
-# number of input variables
-niv <- nrow(input)
-
-# number of output variables
-nov <- nrow(output)
-
-# init nnet
-model <- nnet(7, 3, 2, "sigmoid")
-
-model$weights
-
-
-# train the model
-
-it <- 10000L
-errors <- vector(mode = "numeric", it)
-i <- 1L
-
-while(i <= it) {
-  SSE <- train(model, input, output, learningrate = 0.5)
-  errors[i] <- SSE
-  i <- i + 1L
-}
-
-
-p <- predict(model, input)
-p <- apply(p,2, which.max)
-p <- ifelse(p == 2, 0, 1)
-
-mtcars$am
-p
-# perfect trained model / probably not good ;-)
-
-
-
-# new model
-# ------------------
-
-library(dummies)
-
-input <- mtcars[, 1:7] %>% t %>% unname %>% rescale
-output <- dummy(mtcars$gear) %>% t
-
-unique(mtcars$gear)
-
-model <- nnet(7, 4, 3, "sigmoid")
-model$weights
-
-
-# train the model
-
-it <- 100000L
-errors <- vector(mode = "numeric", it)
-i <- 1L
-
-while(i <= it) {
-  SSE <- train(model, input, output, learningrate = 0.5)
-  errors[i] <- SSE
-  i <- i + 1L
-}
-
-
-
-
-# some errors
-compare <- rbind(apply(predict(model, input),2, which.max), 
-                 apply(dummy(mtcars$gear) %>% t, 2, which.max)) %>% t
-
-
-
-
-
-
-
-
-
 
--- R/testing.R
+++ R/testing.R
@@ -1,26 +1,109 @@
 
-library(pixmap)
-library(data.table)  
-library(nnet)
 
-pixmap::
-img <- read.pnm("img/Number-5-handwritten.png")
+# Testing
+# --------------------------------------------------------------------------------------------------
 
 
-train <- fread("http://www.pjreddie.com/media/files/mnist_train.csv") 
-test <- fread("http://www.pjreddie.com/media/files/mnist_test.csv")
-
-setnames(train, 1, "number")
-setnames(test, 1, "number")
+model <- nnet(4,3,2, "sigmoid")
+model$weights
 
 
-mod <- nnet(number ~ train_pca, data = train, size = 3)
+query(model, c(1,2,3,4))
 
-hidden_layer <- matrix(c(.9, .2, .1,
-                         .3, .8, .5,
-                         .4, .2, .6), nrow = 3)
 
-input <- c(.9, .1, .8)
 
-sigmoid(m %*% input)
+train(model, 1:4, 1:2, 0.1)
+model$weights
+
+# Test Model
+# --------------------------------------------------------------------------------------------------
+
+library(magrittr)
+library(scales)
+
+input <- mtcars[, 1:7] %>% t %>% unname %>% rescale
+output <- mtcars[, "am"]
+output <- rbind(t(output), -1 %*% output + 1)
+row.names(output) <- c(0,1)
+
+
+# number of input variables
+niv <- nrow(input)
+
+# number of output variables
+nov <- nrow(output)
+
+# init nnet
+model <- nnet(7, 3, 2, "sigmoid")
+
+model$weights
+
+
+# train the model
+
+it <- 10000L
+errors <- vector(mode = "numeric", it)
+i <- 1L
+
+while(i <= it) {
+  SSE <- train(model, input, output, learningrate = 0.5)
+  errors[i] <- SSE
+  i <- i + 1L
+}
+
+
+p <- predict(model, input)
+p <- apply(p,2, which.max)
+p <- ifelse(p == 2, 0, 1)
+
+mtcars$am
+p
+# perfect trained model / probably not good ;-)
+
+
+
+# new model
+# ------------------
+
+library(dummies)
+
+input <- mtcars[, 1:7] %>% t %>% unname %>% rescale
+output <- dummy(mtcars$gear) %>% t
+
+unique(mtcars$gear)
+
+model <- nnet(7, 4, 3, "sigmoid")
+model$weights
+
+
+# train the model
+
+it <- 1000L
+errors <- vector(mode = "numeric", it)
+i <- 1L
+
+while(i <= it) {
+  SSE <- train(model, input, output, learningrate = 0.5)
+  errors[i] <- SSE
+  i <- i + 1L
+}
+
+
+# some errors
+compare <- rbind(apply(predict(model, input),2, which.max), 
+                 apply(dummy(mtcars$gear) %>% t, 2, which.max)) %>% t
+
+
+library(ggplot2)
+
+data.frame(errors = errors, i = 1:length(errors)) %>% 
+  ggplot(aes(x = i, y = errors)) +
+  geom_line()
+
+
+## after ore than 500 iterations the erros start to wiggle
+
+
+
+
 
