c053a30d7005214a00903fc17867cfcff94426d6
Dominik Peter <dominikpeter@users.noreply.github.com>
2017-01-04 20:23:42 +0100

    Update nnet2.R

--- R/nnet2.R
+++ R/nnet2.R
@@ -1,87 +1,58 @@
 
-train <- function(model, ...) UseMethod("train")
-query <- function(model, ...) UseMethod("query")
+# get datadata
 
-nnet <- function(inputnodes,
-                      hiddennodes,
-                      outputnodes,
-                      activation_function = c("sigmoid", "tanh")
-                      ) {
-  inodes = inputnodes
-  hnodes = hiddennodes
-  onodes = outputnodes
-  
-  # init random weights
-  wih <- array(rnorm(hnodes*inodes, sd = inodes^-.5), dim = c(hnodes, inodes))
-  who <- array(rnorm(onodes*hnodes, sd = hnodes^-.5), dim = c(onodes, hnodes))
-  
-  activation_function = match.arg(activation_function)
-  
-  structure(list(layer = list(input = inodes,
-                              hidden = hnodes,
-                              output =  onodes),
-                 weights = list(wih = wih, 
-                                who =  who),
-                 activation_function = activation_function),
-            class = "nnet")
+library(data.table)
+library(magrittr)
+library(ggplot2)
+library(scales)
+library(dummies)
+library(foreach)
+library(parallel)
+
+parallel::makeCluster(4)
+
+
+training <- fread("http://pjreddie.com/media/files/mnist_train.csv")
+testing <- fread("http://pjreddie.com/media/files/mnist_test.csv")
+
+
+# training <- fread("datasets/mnist_train_100.csv")
+# testing <- fread("datasets/mnist_test_10.csv")  
+
+setnames(training, c("label", 1:(ncol(training)-1)))
+setnames(testing, c("label", 1:(ncol(testing)-1)))
+
+# init
+length(unique(training$label))
+
+model <- nnet(784, 200, 10, activation_function = "sigmoid")
+
+
+
+it <- 10L
+errors <- vector(mode = "numeric", it)
+i <- 1L
+
+# while(i <= it) {
+#   SSE <- train(model, input, output, learningrate = 0.3)
+#   errors[i] <- SSE
+#   i <- i + 1L
+# }
+
+errors <- array(0, dim = c(20, 784))
+
+foreach(i = 1:784) %dopar% {
+  input <- training[i,-1] %>% as.matrix %>% t %>% rescale(to = 0.01, 0.99)
+  output <- training[i, 1] %>% as.matrix %>% dummy %>% t %>% unname %>% rescale(0.01, 0.99)
+  foreach(t = 1:20) %dopar% {
+    SSE <- train(model, input, output, learningrate = 0.1)
+    errors[t:i] <- SSE
+  }
 }
 
+errors
 
+predict(model, input)
+apply(predict(model, input), 2, which.max)
 
-query.nnet <- function(model, inputs_list) {
-  
-  inputs <- matrix(inputs_list, nrow = model$layer$input)
-  
-  hidden_inputs <- model$weights$wih %*% inputs
-  hidden_outputs <- do.call(model$activation_function,
-                            list(x = hidden_inputs))
-  
-  final_inputs <- model$weights$who %*% hidden_outputs
-  final_outputs <- do.call(model$activation_function,
-                           list(x = final_inputs))
-  
-  list(inputs = inputs,
-       hidden_inputs = hidden_inputs,
-       hidden_outputs = hidden_outputs,
-       final_inputs = final_inputs,
-       final_outputs = final_outputs)
-  
-}
-
-train.nnet <- function(model, inputs_list, target_list, learningrate) {
-  qry_result <- query(model, inputs_list)
-  
-  inputs <- qry_result$inputs
-  targets <- matrix(target_list, nrow = model$layer$output)
-  
-  who <- model$weights$who
-  wih <- model$weights$wih
-  hidden_output <- qry_result$hidden_output
-  final_output <- qry_result$final_output
-  lr <- learningrate
-  
-  output_errors <- targets - final_output
-  hidden_errors <- t(who) %*% output_errors
-  
-  # list(output_errors = output_errors,
-  #      hidden_errors = hidden_errors,
-  #      hidden_output = hidden_output,
-  #      inputs = inputs,
-  #      who = model$weights$who,
-  #      wih = model$weights$wih)
-  
-  model$weights$who <<- model$weights$who + (lr * (output_errors * final_output * (1.0 - final_output)) %*% t(hidden_output))
-  model$weights$wih <<- model$weights$wih + (lr * (hidden_errors * hidden_output * (1.0 - hidden_output)) %*% t(inputs))
-
-  final_error <- targets - query(model, inputs_list)$final_output
-    
-  sum(output_errors^2)
-
-}
-
-
-
-predict.nnet <- function(model, newdata) {
-  query(model, newdata)$final_outputs
-}
-
+testing$label
